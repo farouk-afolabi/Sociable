61f164739008f72e3f9054e2e3a77bc4c33d6d91
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-06-13 01:49:40 -0400

    fix: switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
