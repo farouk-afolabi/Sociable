c25f3374e02873386811330a4afce63193cf7710
Farouk Afolabi <farouk.afolabi@yahoo.com>
2026-04-15 01:34:25 -0400

    updated firebase configurations

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore, collection, onSnapshot } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-  authDomain: "trainlinkit-c98b9.firebaseapp.com",
-  projectId: "trainlinkit-c98b9",
-  storageBucket: "trainlinkit-c98b9.firebasestorage.app",
-  messagingSenderId: "335547332391",
-  appId: "1:335547332391:web:134b8789ec97f1c0badb02",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
